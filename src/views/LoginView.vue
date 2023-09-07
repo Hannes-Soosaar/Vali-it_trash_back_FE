ef4ef0ed0f9fb16052a7827b6dcc96dfa67c89e3
mariakuusik <maria.kuusik@gmail.com>
2023-09-07 15:30:44 +0300

    tegime ära login formi ja nupud login ja registreeri

---
+++ src/views/LoginView.vue
@@ -0,0 +1,30 @@
+
+<template>
+  <div class="container text-center position-absolute top-50 start-50 translate-middle">
+    <div class="row justify-content-center ">
+      <div class="col col-4">
+
+        <div class="form-floating mb-4 ">
+          <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+          <label for="floatingInput">E-posti aadress</label>
+        </div>
+        <div class="form-floating">
+          <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
+          <label for="floatingPassword">Salasõna</label>
+        </div>
+        <div class="mt-2">
+          <button class="btn btn-primary m-2 " type="button">Logi sisse</button>
+          <button class="btn btn-primary m-2" type="button">Registreeri</button>
+        </div>
+
+        </div>
+      </div>
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: "LoginView"
+}
+</script>
--- src/views/SearchView.vue
+++ src/views/SearchView.vue
@@ -5,7 +5,7 @@
         <div class="col col-6">
           <div class="input-group mb-3">
             <input type="text" class="form-control" placeholder="UPC/materjali nimi">
-            <button class="btn btn-outline-secondary" type="button" id="button-addon2">Otsi</button>
+            <button class="btn btn-outline-secondary" type="button" id="button-addon2">Otsi </button>
           </div>
         </div>
       </div>
