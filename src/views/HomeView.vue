5bbfb18e7ce589d1c072c82488f10808f108319d
mariakuusik <maria.kuusik@gmail.com>
2023-09-07 14:56:11 +0300

    search bar tehtud visuaalselt

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,18 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+    <div class="container text-center">
+      <div class="row justify-content-center">
+        <div class="col col-6">
+          <div class="input-group mb-3">
+            <input type="text" class="form-control" placeholder="UPC/materjali nimi">
+            <button class="btn btn-outline-secondary" type="button" id="button-addon2">Otsi</button>
+          </div>
+        </div>
+      </div>
+    </div>
+
   </div>
 </template>
 
@@ -13,6 +24,6 @@ export default {
   name: 'HomeView',
   components: {
     HelloWorld
-  }
+  },
 }
 </script>
