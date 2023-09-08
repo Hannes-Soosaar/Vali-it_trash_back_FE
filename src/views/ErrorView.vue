46eb300a306bc99de61bfd1aaa992cfe27c88eef
TAIRI TIIMANN <tairiti@gmail.com>
2023-09-08 13:07:13 +0300

    ErrorView completed

--- src/views/ErrorView.vue
+++ src/views/ErrorView.vue
@@ -3,7 +3,7 @@
 <template>
   <div class="background">
   <p class="fs-1">Midagi läks valesti</p>
-  <img src="../assets/sadcat.jpg" alt="Sad cat">
+  <img class="img-thumbnail" style="width: 500px; height: 500px" src="../assets/sadcat.jpg" alt="Sad cat">
   </div>
 </template>
 
