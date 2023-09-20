475e2e9059d0e7848d262aad1be4f3b0bb7d0853
TAIRI TIIMANN <tairiti@gmail.com>
2023-09-20 16:44:29 +0300

    Tairi / UPCSearch WIP

--- src/components/UpcSearchResult.vue
+++ src/components/UpcSearchResult.vue
@@ -1,81 +1,57 @@
 <template>
-
-  <div class="container text-start">
-
-    <div class="row justify-content-center">
-
-      <div class="">
-
-        <table class="table ">
-
-          <tbody>
-
-          <tr>
-
-            <td>
-              <div class="justify-content-center">
-                <h5 class="row justify-content-around">Materjal</h5>
-              </div>
-
-            </td>
-
-            <td>
-              <div class="container ">
-                <h5 class="row justify-content-around"> Konteiner </h5>
-              </div>
-            </td>
-
-            <td>
-              <div class="container ">
-                <h5 class="row justify-content-around">Värviga</h5>
-              </div>
-            </td>
-
-          </tr>
-
-          <tr v-for="(material,index) in upcResponseBody.materials" :key="index">
-
-            <td>
-              <div class="container ">
-                <div class="row justify-content-center"> {{ material.materialName }}</div>
-              </div>
-            </td>
-
-            <td>
-              <div class=" container  justify-content-center">
-                <div class="row justify-content-center"> {{ material.materialBinName }}</div>
-              </div>
-            </td>
-
-            <td>
-              <div class="container">
-                <div class="row justify-content-center"> {{ material.materialBinColorName }}</div>
-              </div>
-            </td>
-
-          </tr>
-          </tbody>
-        </table>
-
-      </div>
-
-    </div>
-
-    <div class=" container justify-content-center">
-      <h2 class="row justify-content-center">{{ upcResponseBody.productName }}</h2>
-    </div>
+  <div class="container text-start" style="border: solid 1px grey; border-radius:20px">
+    <h2>{{ upcResponseBody.productName }}</h2>
+    <div> {{ upcResponseBody.productInfo }}</div>
     <div class="container">
-      <div class="row justify-content-evenly"> {{ upcResponseBody.productInfo }}</div>
-    </div>
-    <div class=" col  container">
-      <div class="row m-5">
-        <ProductImage :image-data-base64="upcResponseBody.imageData"/>
+      <div class="row">
+        <div class="col col-6">
+          <table class="table">
+            <tbody>
+            <tr>
+              <td>
+                <div class="justify-content-center">
+                  <h5 class="row justify-content-around">Materjal</h5>
+                </div>
+              </td>
+              <td>
+                <div class="container ">
+                  <h5 class="row justify-content-around"> Konteiner </h5>
+                </div>
+              </td>
+              <td>
+                <div class="container ">
+                  <h5 class="row justify-content-around">Värviga</h5>
+                </div>
+              </td>
+            </tr>
+            <tr v-for="(material,index) in upcResponseBody.materials" :key="index">
+              <td>
+                <div class="container ">
+                  <div class="row justify-content-center"> {{ material.materialName }}</div>
+                </div>
+              </td>
+              <td>
+                <div class=" container  justify-content-center">
+                  <div class="row justify-content-center"> {{ material.materialBinName }}</div>
+                </div>
+              </td>
+              <td>
+                <div class="container">
+                  <div class="row justify-content-center"> {{ material.materialBinColorName }}</div>
+                </div>
+              </td>
+            </tr>
+            </tbody>
+          </table>
+
+        </div>
+
+        <div class="col col-6">
+          <ProductImage :image-data-base64="upcResponseBody.imageData"/>
+        </div>
       </div>
     </div>
-
   </div>
-
-
 </template>
 <script>
 
