406eefa72b92b3d5e987d1100322aed9dbbbc94e
Hannes Soosaar <hannes.study@gmail.com>
2023-09-20 14:55:30 +0300

    HANNES- WIP for search views.

--- src/components/UpcSearchResult.vue
+++ src/components/UpcSearchResult.vue
@@ -1,6 +1,6 @@
 <template>
 
-  <div class="container text-start" style="border: solid 1px red; border-radius:20px">
+  <div class="container text-start">
 
     <div class="row justify-content-center">
 
@@ -10,47 +10,46 @@
 
           <tbody>
 
-          <tr class="" style="border: solid 1px blue; border-radius:20px">
+          <tr>
 
             <td>
-              <div class="justify-content-center" style="border: solid 1px grey; border-radius:20px">
+              <div class="justify-content-center">
                 <h5 class="row justify-content-around">Materjal</h5>
               </div>
 
             </td>
 
             <td>
-              <div class="container " style="border: solid 1px grey; border-radius:20px">
+              <div class="container ">
                 <h5 class="row justify-content-around"> Konteiner </h5>
               </div>
             </td>
 
             <td>
-              <div class="container " style="border: solid 1px grey; border-radius:20px">
+              <div class="container ">
                 <h5 class="row justify-content-around">Värviga</h5>
               </div>
             </td>
 
           </tr>
 
-          <tr v-for="(material,index) in upcResponseBody.materials" :key="index" style="border: solid 1px violet;
-          border-radius:20px">
+          <tr v-for="(material,index) in upcResponseBody.materials" :key="index">
 
             <td>
-              <div class="container " style="border: solid 1px grey; border-radius:20px">
+              <div class="container ">
                 <div class="row justify-content-center"> {{ material.materialName }}</div>
               </div>
             </td>
 
             <td>
-              <div class=" container  justify-content-center" style="border: solid 1px grey; border-radius:20px">
-                <div class="row justify-content-center"> {{ material.materialBinName }} </div>
+              <div class=" container  justify-content-center">
+                <div class="row justify-content-center"> {{ material.materialBinName }}</div>
               </div>
             </td>
 
             <td>
-              <div class="container" style="border: solid 1px grey; border-radius:20px">
-                <div class="row justify-content-center"> {{ material.materialBinColorName }} </div>
+              <div class="container">
+                <div class="row justify-content-center"> {{ material.materialBinColorName }}</div>
               </div>
             </td>
 
@@ -62,15 +61,17 @@
 
     </div>
 
-    <div class=" container justify-content-center" style="border: solid 1px grey; border-radius:20px">
+    <div class=" container justify-content-center">
       <h2 class="row justify-content-center">{{ upcResponseBody.productName }}</h2>
     </div>
-      <div class="container" style="border: solid 1px grey; border-radius:20px; color:darkblue">
-       <div class="row justify-content-evenly"> {{ upcResponseBody.productInfo }}</div>
+    <div class="container">
+      <div class="row justify-content-evenly"> {{ upcResponseBody.productInfo }}</div>
+    </div>
+    <div class=" col  container">
+      <div class="row m-5">
+        <ProductImage :image-data-base64="upcResponseBody.imageData"/>
       </div>
-      <div class=" col  container" style="border: solid 1px grey; border-radius:20px">
-        <div class="row m-5"><ProductImage :image-data-base64="upcResponseBody.imageData"/></div>
-      </div>
+    </div>
 
   </div>
 
